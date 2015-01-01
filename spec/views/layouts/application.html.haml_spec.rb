66998f2d22e542a948abb2a60e750997ad09682f
Evan Keller <evanrkeller@gmail.com>
2014-12-31 23:23:19 -0600

    Refactor view specs to avoid deprication warnings

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "layouts/application" do
 
   before do
-    view.stub(:user_signed_in?) { false }
+    allow(view).to receive(:user_signed_in?) { false }
   end
 
   it "doesn't have an untranslated title" do
     render
-    rendered.should_not match(/<title>t\('app_title'\)<\/title>/)
+    expect(rendered).to_not match(/<title>t\('app_title'\)<\/title>/)
   end
 
   it "has the proper page title" do
@@ -17,7 +17,7 @@ RSpec.describe "layouts/application" do
   end
 
   it "highlights the approprate nav element" do
-    view.stub(:current_page?) { |options| options[:controller] == 'tracks' }
+    allow(view).to receive(:current_page?) { |options| options[:controller] == 'tracks' }
     render
     expect(rendered).to match("<li class='active'>\n<a href=\"/tracks\">Tracks</a>\n</li>")
   end
