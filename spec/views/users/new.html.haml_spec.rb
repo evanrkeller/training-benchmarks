95aebfaf50ca325704b5d07b61a0cebbeef6c71f
Evan Keller <evanrkeller@gmail.com>
2015-01-25 17:00:08 -0600

    Add missing tests for new user

--- spec/views/users/new.html.haml_spec.rb
+++ spec/views/users/new.html.haml_spec.rb
@@ -13,7 +13,10 @@ RSpec.describe "users/new", :type => :view do
 
     assert_select "form[action=?][method=?]", users_path, "post" do
 
+      assert_select "input#user_first_name[name=?]", "user[first_name]"
+      assert_select "input#user_last_name[name=?]", "user[last_name]"
       assert_select "input#user_email[name=?]", "user[email]"
+      assert_select "input#user_phone[name=?]", "user[phone]"
 
       assert_select "select#user_track_id[name=?]", "user[track_id]"
     end
