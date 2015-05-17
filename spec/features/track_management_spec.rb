8f9cf4522b3674a2b328bfbf796798310a421c81
Evan Keller <evanrkeller@gmail.com>
2015-05-17 17:13:47 -0500

    BUGFIX: Destroy doesn't delete a track
    [finishes #94740126]

---
+++ spec/features/track_management_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'Track management', type: :feature do
+  before :each do
+    @user = FactoryGirl.create(:user, email: 'user@example.com', password: 'password')
+    visit '/users/sign_in'
+    within('#new_user') do
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+    end
+    click_button 'Log in'
+  end
+
+  describe 'list of tracks' do
+    it 'deletes a track after the intention is confirmed', js: true do
+      @track = FactoryGirl.create(:track, name: 'Sample')
+      visit tracks_path
+      within(:xpath, "//tr[contains(.,'Sample')]") do
+        click_link 'Destroy'
+      end
+      expect(current_path).to eq tracks_path
+      expect(page).not_to have_text 'Sample'
+    end
+
+    it 'does not delete a track if the intention is not confirmed', js: true do
+      @track = FactoryGirl.create(:track, name: 'Sample')
+      visit tracks_path
+      page.evaluate_script 'window.confirm = function(msg) { return false; }'
+      within(:xpath, "//tr[contains(.,'Sample')]") do
+        click_link 'Destroy'
+      end
+      expect(page).to have_text 'Sample'
+    end
+  end
+end
