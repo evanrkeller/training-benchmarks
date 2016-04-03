6c9441f456448948686d73423cc5d3a67ae6df37
Evan Keller <evanrkeller@gmail.com>
2016-04-03 00:27:03 -0400

    Admin see location names on track index page
    [finishes #116835655]

--- spec/views/tracks/index.html.haml_spec.rb
+++ spec/views/tracks/index.html.haml_spec.rb
@@ -2,18 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'tracks/index', type: :view do
   before(:each) do
-    assign(:tracks, [
-             Track.create!(
-               name: 'Name'
-             ),
-             Track.create!(
-               name: 'Name'
-             )
-           ])
+    @tracks = assign(:tracks, [
+                       Track.create!(
+                         name: 'Name'
+                       ),
+                       Track.create!(
+                         name: 'Name',
+                         location: Location.create!(name: 'Test Location')
+                       )
+                     ])
   end
 
   it 'renders a list of tracks' do
     render
     assert_select 'tr>td', text: 'Name'.to_s, count: 2
+  end
+
+  it "includes a header for the 'Location' column" do
+    render
+    expect(rendered).to have_selector('th', text: Track.human_attribute_name(:location))
+  end
+
+  it 'includes the name of an assigned location' do
+    render
+    expect(rendered).to have_selector('td', text: @tracks.last.location.name)
+  end
+
+  it "says 'No location assigned' for tracks without an assigned location" do
+    render
+    expect(rendered).to have_selector('td', text: t('no_location_assigned'))
   end
 end
