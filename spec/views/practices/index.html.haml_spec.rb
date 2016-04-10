b87eb4aa11fa9287b1b0a0e33dfd234f315b619e
Evan Keller <evanrkeller@gmail.com>
2016-04-10 15:37:31 -0400

    Admin sees stage when viewing a list of practices
    [finishes #117299589]

--- spec/views/practices/index.html.haml_spec.rb
+++ spec/views/practices/index.html.haml_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'practices/index', type: :view do
   before(:each) do
     assign(:practices, [
              FactoryGirl.create(:practice, start_time: '2015-01-01 15:00', end_time: '2015-01-01 17:00'),
-             FactoryGirl.create(:practice)
+             FactoryGirl.create(:practice, stage: FactoryGirl.create(:stage, name: 'Example Stage'))
            ])
   end
 
@@ -18,5 +18,11 @@ RSpec.describe 'practices/index', type: :view do
     expect(rendered).not_to have_selector('th', text: 'Start time')
     expect(rendered).not_to have_selector('th', text: 'End time')
     expect(rendered).to have_selector('td', text: 'January 1, 2015,  3:00 pm- 5:00 pm')
+  end
+
+  it 'displays the stage for a practice' do
+    render
+    expect(rendered).to have_selector('th', text: Practice.human_attribute_name(:stage))
+    expect(rendered).to have_selector('td', text: 'Example Stage')
   end
 end
