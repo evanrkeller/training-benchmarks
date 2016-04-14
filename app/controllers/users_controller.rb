90601ec8188fad8ef189ef6a5e7e03243e5aa491
Evan Keller <evanrkeller@gmail.com>
2016-04-14 16:38:10 -0500

    Admin filters users by track
    [finishes #117542749]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :build_user, only: [:create]
 
+  has_scope :by_track
+
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = apply_scopes(User).all
   end
 
   # GET /users/1
