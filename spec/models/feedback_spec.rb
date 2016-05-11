5ead11e762a571e3fddef9db3689e3fb3ba4f345
Evan Keller <evanrkeller@gmail.com>
2016-05-10 23:44:54 -0500

    Note on feedback is optional
    [finishes #119340491]

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Feedback, type: :model do
   it { should validate_presence_of :user }
   it { should validate_presence_of :practice }
-  it { should validate_presence_of :note }
+  it { should_not validate_presence_of :note }
   it { should belong_to :user }
   it { should belong_to :practice }
   it { should have_many :scores }
